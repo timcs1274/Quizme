1ec4610ba62eb5c3539785f8382ee99574ab1bff
timcs <timcs1274@gmail.com>
2022-08-25 20:41:27 -0400

    add question object

---
+++ assets/index.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+//Array of questions
+var quizQuestions = [
+    {
+        question: 'What does HTML stand for?',
+        options: ['Hyper Time Mark Lapse', 'Hypertext Markup Language', 'Hilltop Merging Logistics'],
+        answer: 'Hypertext Markup Language',
+    },
+    {
+        question: 'What does Javascript do?',
+        options: ['Implement features on web pages', 'Look cool', 'Create client-side servers'],
+        answer: 'Implement features on web pages',
+    },
+    {
+        question: 'What does CSS generally work with?',
+        options: ['An HTML file', 'Mysql', 'Node.js'],
+        answer: 'An HTML file',
+    },
+]
