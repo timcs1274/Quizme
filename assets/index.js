1ce23071e71ddd67a9a703c5a54434b6ce269d82
timcs <timcs1274@gmail.com>
2022-08-26 12:31:18 -0400

    add variables and event listeners for buttons

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,24 @@
+var startBtn = document.getElementById("startQuiz")
+var questionContainerEl = document.getElementById("questionContainer")
+var questionEl = document.getElementById("questions")
+var answerArrEl = document.getElementById("answerArr")
 
+var btn1 = document.getElementById("btn1")
+var btn2 = document.getElementById("btn2")
+var btn3 = document.getElementById("btn3")
+
+var timer = 30;
+var penalty = 3;
+var scoreList = document.getElementById('highscore')
+
+
+startBtn.addEventListener("click", startQuiz) 
+btn1.addEventListener("click", checkAnswer)
+btn1.addEventListener("click", setNextQuestion)
+btn2.addEventListener("click", checkAnswer)
+btn2.addEventListener("click", setNextQuestion)
+btn3.addEventListener("click", checkAnswer)
+btn3.addEventListener("click", setNextQuestion)
 
 
 
